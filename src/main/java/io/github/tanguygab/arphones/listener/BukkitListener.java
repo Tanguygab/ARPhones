8db73a8bef7c4a2ca9a6c96ce6680eb9c7ab6024
Tanguygab <tanguy@gabril.com>
2023-01-07 19:15:27 +0100

    added LOCK_ON_DROP and ALWAYS_LOCKED UnlockModes

--- src/main/java/io/github/tanguygab/arphones/listener/BukkitListener.java
+++ src/main/java/io/github/tanguygab/arphones/listener/BukkitListener.java
@@ -1,6 +1,8 @@
 package io.github.tanguygab.arphones.listener;
 
 import io.github.tanguygab.arphones.ARPhones;
+import io.github.tanguygab.arphones.phone.lock.LockSystem;
+import io.github.tanguygab.arphones.phone.lock.UnlockMode;
 import io.github.tanguygab.arphones.phone.sim.SIMCard;
 import io.github.tanguygab.arphones.menus.PhoneMenu;
 import io.github.tanguygab.arphones.phone.Phone;
@@ -12,10 +14,9 @@ import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.inventory.CraftItemEvent;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.*;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -41,8 +42,41 @@ public class BukkitListener implements Listener {
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent e) {
-        if (ARPhones.get().openedMenus.containsKey(e.getWhoClicked()))
-            e.setCancelled(ARPhones.get().openedMenus.get(e.getWhoClicked()).onClick(e.getCurrentItem(),e.getRawSlot(),e.getClick()));
+        Player p = (Player) e.getWhoClicked();
+
+        if (ARPhones.get().openedMenus.containsKey(p)) {
+            e.setCancelled(ARPhones.get().openedMenus.get(p).onClick(e.getCurrentItem(), e.getRawSlot(), e.getClick()));
+            return;
+        }
+
+        if (e.isCancelled()) return;
+
+
+        ItemStack item = switch (e.getAction()) {
+            case MOVE_TO_OTHER_INVENTORY -> e.getCurrentItem();
+            case PLACE_ONE,PLACE_SOME,PLACE_ALL,SWAP_WITH_CURSOR -> e.getClickedInventory() == e.getView().getTopInventory() ? e.getCursor() : null;
+            case HOTBAR_SWAP,HOTBAR_MOVE_AND_READD -> {
+                if (e.getClickedInventory() != e.getView().getTopInventory()) yield null;
+                if (e.getClick() == ClickType.SWAP_OFFHAND) yield p.getInventory().getItemInOffHand();
+                ItemStack movedItem = e.getView().getBottomInventory().getItem(e.getHotbarButton());
+                if (movedItem == null || movedItem.getType().isAir()) yield movedItem;
+                yield null;
+            }
+            default -> null;
+        };
+
+        Phone phone = Utils.getPhone(item,null);
+        if (phone != null && phone.getLockSystem().getUnlockMode() == UnlockMode.LOCK_ON_DROP)
+            phone.getLockSystem().setLocked(true);
+
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onDrop(PlayerDropItemEvent e) {
+        ItemStack item = e.getItemDrop().getItemStack();
+        Phone phone = Utils.getPhone(item,null);
+        if (phone != null && phone.getLockSystem().getUnlockMode() == UnlockMode.LOCK_ON_DROP)
+            phone.getLockSystem().setLocked(true);
     }
 
     @EventHandler
@@ -50,7 +84,13 @@ public class BukkitListener implements Listener {
         Map<Player,PhoneMenu> menus = ARPhones.get().openedMenus;
         Player p = (Player) e.getPlayer();
         PhoneMenu menu = menus.get(p);
-        if (menu != null && menu.chatInput == null && menu.inv.equals(e.getInventory())) menus.get(p).onClose();
+        if (menu == null || menu.chatInput != null) return;
+        if (menu.inv.equals(e.getInventory())) menus.get(p).onClose();
+        Bukkit.getServer().getScheduler().runTask(ARPhones.get(),()->{
+            LockSystem lock = menu.phone.getLockSystem();
+            if (!menus.containsKey(p) && lock.getUnlockMode() == UnlockMode.ALWAYS_LOCKED)
+                lock.setLocked(true);
+        });
     }
 
     @EventHandler(ignoreCancelled = true)
